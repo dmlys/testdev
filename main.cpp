e4c483455231be75f2aed2aa977c7e372cee0a37
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-09-07 18:11:02 +0300

    dev

--- main.cpp
+++ main.cpp
@@ -3,54 +3,45 @@
 #include <fstream>
 #include <string>
 
-#include <future>
-
-#define BOOST_THREAD_VERSION 4
-#include <boost/thread/future.hpp>
-
 #include <vector>
 #include <random>
 #include <functional>
 #include <algorithm>
 #include <chrono>
 
+#include <variant>
+#include <optional>
+
+#include <ext/itoa.hpp>
 #include <ext/future.hpp>
 #include <ext/enum_bitset.hpp>
 #include <ext/thread_pool.hpp>
 #include <ext/threaded_scheduler.hpp>
+#include <ext/strings/cow_string.hpp>
+
+#include <boost/system/system_error.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <ext/Errors.hpp>
+#include <ext/FileSystemUtils.hpp>
+#include <ext/iostreams/socket_stream.hpp>
+#include <ext/netlib/http_response_parser.hpp>
+#include <ext/netlib/http_response_stream.hpp>
 
 #include <fmt/format.h>
+#include <fmt/ostream.h>
 
-#include <ext/strings/cow_string.hpp>
 
 int main()
 {
 	using namespace std;
 
-	cout << "Boost version: " << BOOST_VERSION << endl;
-	ext::init_future_library(16);
+	auto fi = ext::async(ext::launch::deferred, [] { return 12; });
+	auto ff = fi.then([](auto f) { return f.get() + 100; });
 
-	//_CrtMemState state;
-	//_CrtMemCheckpoint(&state);
-	//
-	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-	//_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+	assert(not ff.has_value());
 
-	//_CrtMemDumpAllObjectsSince(&state);
-
-	auto f1 = ext::async(ext::launch::deferred, [] { return 12; }).share();
-	ext::future<int> f2;
-
-	{
-		ext::thread_pool pool {1};
-		f2 = pool.submit(f1, [](auto f) { return f.get() + 12; });
-	}
-
-	f2.wait();
-	assert(f2.is_abandoned());
-	
-	f1.wait();
-	cout << f1.get() << endl;
-	
-	return 0;
+	auto res = ff.get();
+	cout << res << endl;
+	return res;
 }
